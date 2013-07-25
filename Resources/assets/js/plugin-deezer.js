881640be725f6107220805ce12c9a30eee148268
plfort <plfort@cogipix.com>
2013-07-25 23:46:02 +0200

    fix play next song

--- Resources/assets/js/plugin-deezer.js
+++ Resources/assets/js/plugin-deezer.js
@@ -1,5 +1,64 @@
 
+var nextDeezerTimeout = null;
 
+function initCogimixDeezer(){
+	DZ.Event.subscribe('player_buffering', function(evt){
+		musicPlayer.cursor.progressbar('value',evt);
+		  });
+
+	DZ.Event.subscribe('player_paused', function(evt_name){
+		loggerDeezer.debug('player_paused');
+		musicPlayer.unbinCursorStop();
+	});
+
+
+
+	DZ.Event.subscribe('player_play', function(evt_name){
+		loggerDeezer.debug('player_play');
+		//DZ.Event.unsubscribe('player_position');
+		DZ.Event.subscribe('player_position', function(args,evt_name){
+
+			loggerDeezer.debug('player_position '+args[0]+' '+(args[1]));
+
+			if(args[1]>0 && args[0]>=(args[1]-2) && nextDeezerTimeout ==null){
+				//DZ.Event.unsubscribe('player_position');
+				nextDeezerTimeout = setTimeout(function(){
+					try{
+						DZ.Event.unsubscribe('player_position');
+						musicPlayer.plugin['dz'].pause();
+						musicPlayer.next();
+					}catch(err){
+						loggerDeezer.error(err);
+					}
+				},2000);
+			
+			}
+			if(musicPlayer.cursor.data('isdragging')==false){
+			  musicPlayer.cursor.slider("value", args[0]);
+			}
+		});
+		musicPlayer.enableControls();
+
+		musicPlayer.bindCursorStop(function(value) {
+		
+			try{
+				DZ.player.seek(value*100/musicPlayer.plugin['dz'].currentDuration);
+			}catch(err){
+				loggerDeezer.error(err);
+			}
+		});
+	});
+
+
+
+	DZ.Event.subscribe('current_track', function(args, evt_name){
+		loggerDeezer.debug('current track change');
+		nextDeezerTimeout = null;
+		musicPlayer.plugin['dz'].currentDuration=args.track.duration;
+		musicPlayer.cursor.slider("option", "max", musicPlayer.plugin['dz'].currentDuration).progressbar();
+	});
+	
+}
 function deezerPlayer(musicPlayer){
 this.name = "Deezer";
 this.cancelRequested=false;
@@ -11,11 +70,11 @@ this.currentDuration=0;
 this.widgetElement =$("#deezerWidgetContainer");
 var self = this;
 
+initCogimixDeezer();
+
 this.requestCancel=function(){
 	self.cancelRequested=true;
 };
-
-
 
 
 this.play = function(item) {
