5ee3244ad1c7809aec5d6214735d9a01d21f6bc5
plfort <plfort@cogipix.com>
2014-03-09 23:30:28 +0100

    new menu

--- ViewHooks/Menu/MenuItem.php
+++ ViewHooks/Menu/MenuItem.php
@@ -1,17 +1,22 @@
 <?php
 namespace Cogipix\CogimixDeezerBundle\ViewHooks\Menu;
 use Cogipix\CogimixCommonBundle\ViewHooks\Menu\MenuItemInterface;
+use Cogipix\CogimixCommonBundle\ViewHooks\Menu\AbstractMenuItem;
 
 /**
  *
  * @author plfort - Cogipix
  *
  */
-class MenuRenderer implements MenuItemInterface{
+class MenuItem extends AbstractMenuItem{
 
     public function getMenuItemTemplate()
     {
           return 'CogimixDeezerBundle:Menu:menu.html.twig';
 
+    }
+
+    public function getName(){
+    	return 'deezer';
     }
 }
